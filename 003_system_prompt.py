412d09cb9805622f8e00d7d190351802ee5787d8
Jayden Houghton <jayden.hou12@gmail.com>
2026-08-17 16:03:28 +1200

    System prompt example

---
+++ 003_system_prompt.py
@@ -0,0 +1,54 @@
+from dotenv import load_dotenv
+from anthropic import Anthropic
+
+
+# Setup
+load_dotenv()
+
+
+# Create API client
+client = Anthropic()
+model = "claude-haiku-4-5-20251001"
+
+
+# Helper functions
+def add_user_message(messages, text):
+    user_message = {"role": "user", "content": text}
+    messages.append(user_message)
+
+def add_assistant_message(messages, text):
+    assistant_message = {"role": "assistant", "content": text}
+    messages.append(assistant_message)
+
+
+# Main request
+def chat(messages, system=None):
+    params = {
+        "model": model,
+        "max_tokens": 1000,
+        "messages": messages,
+    }
+
+    if system:
+        params["system"] = system
+    
+    # Make a request
+    message = client.messages.create(**params);
+    return message.content[0].text
+
+
+messages = [] # Define history list
+system_prompt = """
+    You are a math tutor. Do not directly answer student's questions but 
+    guide them to the answer step by step.
+"""
+
+
+while True:
+    user_input = input("Prompt? > ")
+    print("> ", user_input)
+    add_user_message(messages, user_input)
+    response = chat(messages, system=system_prompt) # Send request to API
+    add_assistant_message(messages, response) # Record response in history  
+    print("Response > ", response.content[0].text)
+
